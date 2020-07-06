de14dbfdc63d6a604b9a243e7731ac0310e1c697
randikaperera96 <randikaperera135@gmail.com>
2020-07-06 18:05:29 +0530

    Async variable issue demo

--- random.js
+++ random.js
@@ -1,12 +1,14 @@
 console.log("1: Before calling DB ...");
 
-function getMovieDataFromDb(){
-    setTimeout(()=>{
+function getMovieDataFromDb() {
+    setTimeout(() => {
         console.log("2: Reading movie data from our DB ...");
-        return {id: 30, name: 'Avengers : End Game'};
+        return { id: 30, name: 'Avengers : End Game' };
     }, 4000);
 }
 
-getMovieDataFromDb();
+let movieDataFromDB = getMovieDataFromDb();
+
+console.log('3: Movie Data : ' + movieDataFromDB);
 
 console.log('3: Doing some other work now ...');
