eaa29af91ec298cfeaaa5ff0b710a84c41c5dd78
randikaperera96 <randikaperera135@gmail.com>
2020-07-09 17:40:35 +0530

    Commit progress upto 20200709

--- random.js
+++ random.js
@@ -15,10 +15,17 @@ function getMovieDataFromDb() {
 
 // console.log('3: Movie Data : ' + movieDataFromDB);
 
-//--Using resolved Promise
-getMovieDataFromDb().then(function (result) {
-    let movieDataFromDB = result;
+// //--Using resolved Promise
+// getMovieDataFromDb().then(function (result) {
+//     let movieDataFromDB = result;
+//     console.log('3: Movie Data : ' + JSON.stringify(movieDataFromDB));
+// })
+
+async function printMovieDetails() {
+    let movieDataFromDB = await getMovieDataFromDb();
     console.log('3: Movie Data : ' + JSON.stringify(movieDataFromDB));
-})
+}
+
+printMovieDetails();
 
 console.log('4: Doing some other work now ...');
