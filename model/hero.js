25b05d25f153b54f4d7e05da983b7bbdb85b9432
randikaperera96 <randikaperera135@gmail.com>
2020-07-20 18:00:02 +0530

    Mongo DB demo upto get ,commit work on 20200713

--- model/hero.js
+++ model/hero.js
@@ -1,11 +1,22 @@
 const mongoose = require('mongoose');
 
 const heroSchema = new mongoose.Schema({
-    neame: String,
+    name: {
+        type: String,
+        minlength: 4,
+        maxlengthA: 20,
+        required: true
+    },
     birthName: String,
-    movies: String,
+    movies: {
+        type: [String],
+        enum: ["infinity War", "Endgame", "Iron Man 2", "The First Avenger"]
+    },
     likeCount: Number,
-    imgURL: String,
+    imgURL: {
+        type:String,
+        default:"Placeholder Image to be updated here..."
+    },
     deceased: Boolean
 });
 
--- route/heroes.js
+++ route/heroes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { Router } = require('express');
+const Hero = require('../model/hero');
 // const app = express();// top level function of express [ similar to "costructor" in OOP ]
 const router = express.Router();// to do route handling on an external file/ modules
 
@@ -22,12 +23,34 @@ let heroArrray = [
     }
 ];
 
-
+/*
 // router.get("/api/heroes", (req, res) => {
 router.get("/", (req, res) => {
     // let heroes = ["Captain America", "IRON MAN", "HULK", "Quick Silver"];
     // res.send(heroes);
     res.send(heroArrray);
+});*/
+
+router.get("/", async (req, res) => {
+    let heroes = await Hero.find();
+    // let heroes = await Hero.find({ deceased: true });
+    //let heroes = await Hero.find().limit(1);
+    // let heroes = await Hero.find().sort({ name: 1 });
+    // let heroes = await Hero.find()
+    //     .sort({ name: 'asc' })
+    //     .select({ name: 1, deceased: 1 });
+    // let heroes = await Hero.find({ likeCount: { $nin: 3000 } }); // '$nin' not in 
+    // let heroes = await Hero.find({ likeCount: { $nin: [3000, 5000] } });
+    // let heroes = await Hero.find()
+    //     .or([{ likeCount: 3000 }, { likeCount: 5000 }])
+    //     .select({ name: 1, deceased: 1 });
+    // let heroes = await Hero.find()
+    //     .and([{ likeCount: 3000 }, { likeCount: 5000 }])
+    //     .select({ name: 1, deceased: 1 });
+    // let heroes = await Hero.find()
+    //     .or([{ likeCount: 3000 }, { likeCount: 5000 }])
+    //     .select({ name: 1, deceased: 1 }).count();
+    res.send(heroes);
 });
 
 // myy example
@@ -50,7 +73,7 @@ router.get("/", (req, res) => {
 //     res.send(hero);
 // });
 
-// router.get("/api/heroes/:heroId", (req, res) => {  // EX: /api/heroes/1
+/*// router.get("/api/heroes/:heroId", (req, res) => {  // EX: /api/heroes/1
 router.get("/:heroId", (req, res) => {
     let heroes = ["Captain America", "IRON MAN", "HULK", "Quick Silver"];
     let heroId = parseInt(req.params.heroId);
@@ -63,8 +86,20 @@ router.get("/:heroId", (req, res) => {
         res.send(hero);
     }
 });
+*/
+router.get("/:heroId", async (req, res) => {
+    let heroId = req.params.heroId;
 
-// router.post('/api/heroes', (req, res) => {
+    let hero = await Hero.findById(heroId);
+
+    if (!hero) {
+        res.status(404).send({ message: "No hero under requested id" });
+    } else {
+        res.send(hero);
+    }
+});
+
+/*// router.post('/api/heroes', (req, res) => {
 router.post('/', (req, res) => {
     let newHeroName = req.body.heroName;
 
@@ -81,8 +116,38 @@ router.post('/', (req, res) => {
     heroArrray.push(newHeroObj);
     console.log(heroArrray);
     res.send(newHeroObj);
+});*/
+
+router.post('/', async (req, res) => {
+    let heroName = req.body.heroName;
+    let birthName = req.body.birthName;
+    let movies = req.body.movies;
+    let likeCount = req.body.likeCount;
+    let imgUrl = req.body.imgUrl;
+    let deceased = req.body.deceased;
+
+    if (!heroName) {
+        return res.status(400).send("Mandatory Fields Not Given! Check API documentation");
+    }
+
+    try {
+        let heroTobeAddedToDb = new Hero({
+            name: heroName,
+            birthName: birthName,
+            movies: movies,
+            likeCount: likeCount,
+            imgURL: imgUrl,
+            deceased: deceased
+        });
+
+        heroTobeAddedToDb = await heroTobeAddedToDb.save();
+        res.send(heroTobeAddedToDb);
+    } catch (e) {
+        return res.status(500).send(e.message);
+    }
 });
 
+/*
 // router.put('/api/heroes/:heroId', (req, res) => {
 router.put('/:heroId', (req, res) => {
     let heroId = parseInt(req.params.heroId);
@@ -102,6 +167,30 @@ router.put('/:heroId', (req, res) => {
     hero.name = newHeroName;
     console.log(heroArrray);
     res.send(hero);
+});
+*/
+
+//basic
+router.put('/:heroId', async (req, res) => {
+    let heroId = req.params.heroId;
+    let heroName = req.body.heroName;
+
+    if (!heroName) {
+        return res.status(400).send("Mandatory Fields Not Given! Check API documentation");
+    }
+
+
+    let hero = await Hero.findById(heroId);
+    try {
+        hero.set({
+            name: heroName
+        });
+
+        hero = hero.save();
+        res.send(heroTobeAddedToDb);
+    } catch (e) {
+        return res.status(500).send(e.message);
+    }
 });
 
 // my attempt
